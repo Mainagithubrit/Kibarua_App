101bb20d3b2b9cb13abd189cd316c45558afc859
Mainagithubrit <njorogefrancismaina@gmail.com>
2025-01-22 16:11:45 +0300

    Kibarua_app

--- kibarua/app/__init__.py
+++ kibarua/app/__init__.py
@@ -2,12 +2,31 @@
 
 from flask import Flask
 from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb://localhost:27017/kibarua_db"
-mongo = PyMongo(app)
-
+from flask_mail import Mail
+from .config_passwrd import Email, password
 from .config import Config
-from . import routes
+mongo = PyMongo()
+mail = Mail()
 
-app.config.from_object(Config)
+
+def create_app():
+    """a funtion that creates my app"""
+    app = Flask(__name__)
+    app.config['MONGO_URI'] = "mongodb://localhost:27017/kibarua_db"
+
+    app.config.from_object(Config)
+
+    app.config['MAIL_SERVER']= "smtp.gmail.com"
+    app.config['MAIL_PORT']= 465
+    app.config['MAIL_USERNAME'] = Email
+    app.config['MAIL_PASSWORD'] = password
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] =True
+
+    mongo.init_app(app)
+    mail.init_app(app)
+
+    with app.app_context():
+        from .routes import register_routes
+        register_routes(app)
+    return app
--- kibarua/app/routes.py
+++ kibarua/app/routes.py
@@ -1,82 +1,95 @@
 """This handles the routes for the app"""
 from flask import render_template, request, url_for, redirect, session
 from flask_pymongo import PyMongo
-from . import app, mongo
+from . import mongo, mail
 import bcrypt
+from flask_mail import Message
+from .config_passwrd import Email, password
 
 
-app.config['STRICT_SLASHES'] = False
+def register_routes(app):
+    @app.route('/')
+    @app.route('/index', methods=['GET', 'POST'])
+    def index():
+        """This handles the Home route and sends an email"""
+        if request.method == "POST":
+            name = request.form.get('name')
+            email = request.form.get('email')
+            message = request.form.get('message')
+
+            msg = Message(subject=f"Mail from: {name}",
+                          body=f"""
+                          Name: {name}\n
+                          E-mail: {email}\n\n\n
+                          Message: {message}""",
+                          sender = Email,
+                          recipients =["njorogefrancismaina@gmail.com"])
+            mail.send(msg)
+            return render_template('index.html', success=True)
+        return render_template('index.html', title='Home')
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    """This handles the Home route"""
-    return render_template('index.html', title='Home')
+    @app.route('/login', methods=["GET", 'POST'])
+    def login():
+        """This handles the login route"""
+        if request.method == 'POST':
+            users = mongo.db['users']
+            username = request.form.get('username')
+            password = request.form.get('passwrd')
 
+            if not username or not password:
+                return "Username and password are required", 400
 
-@app.route('/login', methods=["GET", 'POST'])
-def login():
-    """This handles the login route"""
-    if request.method == 'POST':
-        users = mongo.db['users']
-        username = request.form.get('username')
-        password = request.form.get('passwrd')
+            username_exists = users.find_one({'username': username})
 
-        if not username or not password:
-            return "Username and password are required", 400
-
-        username_exists = users.find_one({'username': username})
-
-        if (
+            if (
                 username_exists
                 and bcrypt.checkpw(
                     password.encode('utf-8'),
                     username_exists['password']
-                    )
+                )
             ):
-            session['username'] = username
-            return redirect(url_for('choice'))
-        return 'Invalid username, email and password', 400
-    return render_template('login.html', title='Login Page', view='login')
+                session['username'] = username
+                return redirect(url_for('choice'))
+            return 'Invalid username, email and password', 400
+        return render_template('login.html', title='Login Page', view='login')
 
 
-@app.route('/signup', methods=('GET', 'POST'))
-def signup():
-    """This handles the sugnup route"""
-    if request.method == 'POST':
-        users = mongo.db['users']
-        fullname = request.form.get('fullname')
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('passwrd')
+    @app.route('/signup', methods=('GET', 'POST'))
+    def signup():
+        """This handles the sugnup route"""
+        if request.method == 'POST':
+            users = mongo.db['users']
+            fullname = request.form.get('fullname')
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('passwrd')
 
-        if not all([fullname, username, email, password]):
-            return "All fields are required", 400
-        
-        user_exists = users.find_one({'username': username })
+            if not all([fullname, username, email, password]):
+                return "All fields are required", 400
+            user_exists = users.find_one({'username': username })
 
-        if user_exists:
-            return "Username already exists!", 400
-            
-        hashpass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        users.insert_one({
-            'fullname': fullname,
-            'username': username,
-            'email':email,
-            'password': hashpass
+            if user_exists:
+                return "Username already exists!", 400
+
+            hashpass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            users.insert_one({
+                'fullname': fullname,
+                'username': username,
+                'email':email,
+                'password': hashpass
             })
-        return redirect(url_for('login'))
-    return render_template('signup.html', title='Sign In Page', view='signup,')
+            return redirect(url_for('login'))
+        return render_template('signup.html', title='Sign In Page', view='signup,')
 
 
-@app.route("/choice")
-def choice():
-    """This handles the choice page for someone to choose whether to be
-    a client or a skilled woker"""
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    return render_template('choice.html', title='Choice', view='choice')
+    @app.route("/choice")
+    def choice():
+        """This handles the choice page for someone to choose whether to be
+        a client or a skilled woker"""
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return render_template('choice.html', title='Choice', view='choice')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(debug=True)
